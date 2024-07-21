d3d5592912ec8cea65a25026e39f1b96a0ce5a9e
cedrical122305 <kucinghutam122305@gmail.com>
2024-07-22 00:00:17 +0700

    balek sebelum mencoba;)

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("sql300.infinityfree.com","if0_36167054","iIlF5L4hB8L9","if0_36167054_yourpuskes");
+    $conn = mysqli_connect("localhost","root","","web_puskesmas");
     if (!$conn) {
         die("Koneksi gagal: " . mysqli_connect_error());
     }

592344e6795d124a37d2b029797123e442b577d2
cedrical122305 <kucinghutam122305@gmail.com>
2024-07-21 23:44:34 +0700

    coba gantiproses_login.php

--- proses/proses_login.php
+++ proses/proses_login.php
@@ -1,22 +1,33 @@
 <?php
 session_start();
 include "connect.php";
-    $username = (isset($_POST['username'])) ? htmlentities($_POST['username']) : "";
-    $password = (isset($_POST['password'])) ? md5(htmlentities($_POST['password'])) : "";
-    if(!empty($_POST['submit_validate'])){
-        $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' && 
-        password = '$password'");
-        $hasil = mysqli_fetch_array($query);
-        if ($hasil){
-            $_SESSION['username_puskesmas'] = $username;
-            $_SESSION['level_puskesmas'] = $hasil['level'];
-            header('location:../home');
-        }  else{?>
-                <script>
-                    alert('Username atau Password yang anda masukkan tidak valid');
-                    window.location='../login'
-                </script>
-<?php
+
+$username = isset($_POST['username']) ? htmlentities($_POST['username']) : "";
+$password = isset($_POST['password']) ? md5(htmlentities($_POST['password'])) : "";
+
+if (!empty($_POST['submit_validate'])) {
+    // Periksa apakah tabel `tb_user` ada
+    $table_check_query = "SHOW TABLES LIKE 'tb_user'";
+    $table_result = mysqli_query($conn, $table_check_query);
+    
+    if (mysqli_num_rows($table_result) == 0) {
+        die("Table 'tb_user' doesn't exist.");
+    }
+
+    $query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' AND password = '$password'");
+    $hasil = mysqli_fetch_array($query);
+
+    if ($hasil) {
+        $_SESSION['username_puskesmas'] = $username;
+        $_SESSION['level_puskesmas'] = $hasil['level'];
+        header('location:../home');
+    } else {
+        ?>
+        <script>
+            alert('Username atau Password yang anda masukkan tidak valid');
+            window.location='../login';
+        </script>
+        <?php
     }
 }
 ?>
